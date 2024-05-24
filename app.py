bdb3d6b3c92a6c2220b34a5b02440a39aa711dbf
Crazy Python <76658093+crazypythonista@users.noreply.github.com>
2024-05-24 19:03:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+import os
 
 hf_token = st.secrets["HUGGINGFACE_TOKEN"]
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = hf_token
@@ -45,7 +46,6 @@ def get_vectorstore(text_chunks):
 def get_conversation_chain(vectorstore):
     # llm = ChatOpenAI()
     llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
